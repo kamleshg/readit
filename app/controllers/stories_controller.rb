019e8322085495d656bd03d978cf8da6cbb6ecf9
Kamlesh Gokal <kamlesh.gokal@calabrio.com>
2018-09-20 23:12:12 -0400

    Added Stories create

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,22 @@
 class StoriesController < ApplicationController
   def index
+    @current_time = Time.now
+
+    @story = Story.order('RANDOM()').first
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+    @story.save
+    redirect_to stories_path
+  end
+
+  private
+  def story_params
+    params.require(:story).permit(:name, :link)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_153855) do
+ActiveRecord::Schema.define(version: 2018_09_21_013031) do
 
   create_table "stories", force: :cascade do |t|
     t.string "name"
